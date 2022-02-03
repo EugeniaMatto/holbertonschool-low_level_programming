b6793cd27033caf64e235fb161e5af581f35de6b
EugeniaMatto <eugematto26@gmail.com>
2022-02-03 08:06:31 -0800

    hola

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Ejercicio 4
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
