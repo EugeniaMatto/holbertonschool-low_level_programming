463ecb4d67e1b1a3d9a27e87b7743759f55255c2
EugeniaMatto <eugematto26@gmail.com>
2022-02-04 09:37:26 -0800

    ultimo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,25 +14,26 @@ for (a = 0 ; a <= 9; a++)
 {
 for (b = 0 ; b <= 9 ; b++)
 {
-if ((a != b) && (a < b))
+if (a <= b)
 {
-putchar(a % 10 + '0');
-putchar(b % 10 + '0');
 for (c = 0 ; c <= 9; c++)
 {
 for (d = 0 ; d <= 9 ; d++)
 {
-if ((c != d) && (c < d))
+if (c < d)
 {
+putchar(a % 10 + '0');
+putchar(b % 10 + '0');
+putchar(' ');
 putchar(c % 10 + '0');
 putchar(d % 10 + '0');
-}
-}
-}
-if (a != 9 && b != 8)
+if (a != 9 || b != 8)
 {
 putchar(',');
 putchar(' ');
+}
+}
+}
 }
 }
 }
