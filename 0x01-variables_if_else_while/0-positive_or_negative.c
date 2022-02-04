cb72f868e2a414ffa856144fd4cedff04ed08029
EugeniaMatto <eugematto26@gmail.com>
2022-02-04 05:57:34 -0800

    0,1,2 listo#

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - La funcion de inicio
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+printf("%d", n);
+if (n > 0)
+{
+printf(" is positive \n");
+}
+else if (n < 0)
+{
+printf(" is negative \n");
+}
+else
+{
+printf(" is zero \n");
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
