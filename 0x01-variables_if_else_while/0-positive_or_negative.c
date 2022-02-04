b7c82b3b90bb6e420b9ee32703e1c368126641c5
EugeniaMatto <eugematto26@gmail.com>
2022-02-04 07:24:36 -0800

    fix ej 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 printf("%d", n);
 if (n > 0)
 {
-printf(" is positive \n");
+printf(" is positive\n");
 }
 else if (n < 0)
 {
-printf(" is negative \n");
+printf(" is negative\n");
 }
 else
 {
-printf(" is zero \n");
+printf(" is zero\n");
 }
 return (0);
 }
