92a8232a791755b7323b3b09124cbfe43e7e1b93
EugeniaMatto <eugematto26@gmail.com>
2022-02-07 04:13:44 -0800

    exe 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - asdas
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+char string[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+putchar(string[i]);
+}
+putchar('\n');
+return (0);
+}
