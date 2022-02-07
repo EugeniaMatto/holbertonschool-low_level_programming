1af3d52a20016284aacc9a66ce56b9350cb2ddb7
EugeniaMatto <eugematto26@gmail.com>
2022-02-07 10:02:36 -0800

    11 fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,22 +14,19 @@ if (n < 0)
 i = -n;
 _putchar('-');
 }
+if (n <= -100)
+{
+_putchar (i / 10 + '0');
+i = i - 100;
+}
 if (n <= -10)
-{
 _putchar(i / 10 + '0');
-}
 if (n < 0)
-{
 _putchar(i % 10 + '0');
-}
 if (n > 10)
-{
 _putchar(n / 10 + '0');
-}
 if (n >= 0)
-{
 _putchar(n % 10 + '0');
-}
 if (n != 98)
 {
 _putchar(',');
