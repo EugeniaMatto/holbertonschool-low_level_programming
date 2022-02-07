f3ea1112b08ac44b43c5e8d188a50a6aed29c4fe
EugeniaMatto <eugematto26@gmail.com>
2022-02-07 08:33:46 -0800

    exe 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - asda
+ *
+ */
+void times_table(void)
+{
+int i;
+int a;
+int res;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (a = 0 ; a <= 9 ; a++)
+{
+res = i * a;
+if (res >= 10)
+{
+_putchar(res / 10 + '0');
+}
+_putchar(res % 10 + '0');
+if (a != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+}
