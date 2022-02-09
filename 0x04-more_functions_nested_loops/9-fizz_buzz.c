5e7bc9e29a1a0710a7da6b1d4bf385021a095afb
EugeniaMatto <eugematto26@gmail.com>
2022-02-09 07:38:07 -0800

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - asd
+ * Return: sad
+ */
+int main(void)
+{
+int i;
+int x = 0;
+for (i = 1; i <= 100; i++)
+{
+if (((i % 10) + (i / 10)) % 3 == 0)
+{
+printf("Fizz");
+x = 1;
+}
+if ((i % 10 == 5) || (i % 10 == 0))
+{
+printf("Buzz");
+x = 1;
+}
+if (x == 0)
+printf("%d", i);
+putchar(' ');
+x = 0;
+}
+putchar('\n');
+return (0);
+}
