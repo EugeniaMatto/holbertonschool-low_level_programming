bdf610bd3515f837ab8db9f449358e37e87bb4ed
EugeniaMatto <eugematto26@gmail.com>
2022-02-11 09:37:48 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - ad
+ * @s: string
+ */
+void print_rev(char *s)
+{
+int i, cont = 0;
+char l = '0';
+while (l != '\0')
+{
+	cont++;
+	l = *(s + cont);
+}
+
+for (i = cont - 1 ; i >= 0; i--)
+{
+_putchar(*(s + i));
+}
+_putchar('\n');
+}
