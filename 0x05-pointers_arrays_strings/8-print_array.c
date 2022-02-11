fa452ce44e082089b5a9b36c8786286ac20378ba
EugeniaMatto <eugematto26@gmail.com>
2022-02-11 12:44:53 -0800

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - dasd
+ * @a: ad
+ * @n: ads
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != (n - 1))
+printf(" ,");
+}
+putchar('\n');
+}
