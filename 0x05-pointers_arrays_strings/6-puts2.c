ed363a631317d6b41f8bc776f61f4a6a6e2c1193
EugeniaMatto <eugematto26@gmail.com>
2022-02-11 11:56:50 -0800

    s

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+int _putchar(char c);
+/**
+ * puts2 - ad
+ * @s: string
+ */
+void puts2(char *s)
+{
+int largo = _strlen(s);
+int i;
+for (i = 0 ; i < largo; i++)
+{
+	if (i % 2 == 0)
+	_putchar(i + '0');
+}
+}
+/**
+ * _strlen - ada
+ * @s: das
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int cont = 0;
+char l = '0';
+if (*s == '\0')
+return (0);
+while (l != '\0')
+{
+cont++;
+l = *(s + cont);
+}
+return (cont);
+}
