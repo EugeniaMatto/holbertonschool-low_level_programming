35c9abe0593833462422c8c4401f7f61c2428861
EugeniaMatto <eugematto26@gmail.com>
2022-02-11 12:13:26 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * puts_half - ad
+ * @s: string
+ */
+void puts_half(char *str)
+{
+int largo = _strlen(str);
+int i = 0;
+if (largo % 2 == 1)
+{
+largo = largo / 2;
+for (i = 0; i < largo; i++)
+	{
+	_putchar(*(str + i));
+	}
+}
+else
+{
+for (i = largo / 2 ; i < largo; i++)
+{
+ _putchar(*(str + i));
+}
+}
+_putchar('\n');
+}
+/**
+ * _strlen - ada
+ * @s: das
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int cont = 0;
+char l = '0';
+if (*s == '\0')
+return (0);
+while (l != '\0')
+{
+cont++;
+l = *(s + cont);
+}
+return (cont);
+}
