bb142ec970e7d93c7714b0e4d46b011a086a465e
EugeniaMatto <eugematto26@gmail.com>
2022-02-11 11:41:59 -0800

    5 fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,34 +6,28 @@ int _putchar(char c);
  */
 void rev_string(char *s)
 {
-	int largo = _strlen(s);
-	int b, i = 0;
-	char string[] = "";
-	i = largo - 1;
-	for (b = 0 ; b < i; b++)
-	{
-		string[0] = s[i];
-		s[i] = s[b];
-	        s[b] = string[0];	
-		i--;
-	}
+int largo = _strlen(s);
+int b, i = 0;
+char string[] = "";
+i = largo - 1;
+for (b = 0 ; b < i; b++)
+{
+string[0] = s[i];
+s[i] = s[b];
+s[b] = string[0];	
+i--;
 }
-
-/**
- * _strlen - retorna el largo
- * @s: char
- * Return: largo
- */
+}
 int _strlen(char *s)
 {
-	int cont = 0;
-	char l = '0';
-	if (*s == '\0')
-		        return (0);
-	while (l != '\0')
-	{
-		cont++;
-		l = *(s + cont);
-	}
-	return (cont);
+int cont = 0;
+char l = '0';
+if (*s == '\0')
+return (0);
+while (l != '\0')
+{
+cont++;
+l = *(s + cont);
+}
+return (cont);
 }
