08108c86c703802a20bc66bad1c269d6ab2e4587
EugeniaMatto <eugematto26@gmail.com>
2022-02-15 09:17:03 -0800

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * leet - codifica
+ * @a: cadena a codificar
+ */
+char *leet(char *a)
+{
+int i, x = 0;
+char letras[] = {'a','A','e','E','o','O','t','T','l','L'};
+char numeros[] = {'4','4','3','3','0','0','7','7','1','1'};
+	while (a[i] != '\0')
+	{
+		for (x = 0; x < 10; x++)
+		{
+		if (a[i] == letras[x])
+			a[i] = numeros[x];
+		}
+	i++;
+	}
+	return (a);
+}
