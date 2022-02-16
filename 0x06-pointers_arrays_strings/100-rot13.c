dafeb59f4677b909599bc605795d92aca1899a00
EugeniaMatto <eugematto26@gmail.com>
2022-02-16 05:41:38 -0800

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,15 +2,16 @@
 /**
  * rot13 - codifica en rot13
  * @a: cadena a codificar
+ * Return: caden en rot13
  */
 char *rot13(char *a)
 {
 int i;
 for (i = 0; a[i] != '\0'; i++)
 {
-while ((a[i] >= 'a' && a[i] <= 'z')||(a[i] >= 'A' && a[i] <= 'Z'))
+while ((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
 {
-if ((a[i] >= 'a' && a[i] <= 'm')||(a[i] >= 'A' && a[i] <= 'M'))
+if ((a[i] >= 'a' && a[i] <= 'm') || (a[i] >= 'A' && a[i] <= 'M'))
 {
 a[i] = a[i] + 13;
 break;
