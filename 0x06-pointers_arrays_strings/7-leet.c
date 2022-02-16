7a599cda64ba61e4881d2bc3ff0b0973720111b2
EugeniaMatto <eugematto26@gmail.com>
2022-02-16 04:28:29 -0800

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,8 +6,8 @@
 char *leet(char *a)
 {
 int i, x = 0;
-char letras[] = {'a','A','e','E','o','O','t','T','l','L'};
-char numeros[] = {'4','4','3','3','0','0','7','7','1','1'};
+char letras[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+char numeros[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	while (a[i] != '\0')
 	{
 		for (x = 0; x < 10; x++)
