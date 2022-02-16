2c37ed7d65804e606d6db195655ba0d053077715
EugeniaMatto <eugematto26@gmail.com>
2022-02-16 04:33:26 -0800

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,7 @@ int _strlen(char *s);
  */
 int _strcmp(char *s1, char *s2)
 {
-if (_strlen(s1) > _strlen(s2))
-{
-return (15);
-}
-else if (_strlen(s2) > _strlen(s1))
-{
-return (-15);
-}
-else
-{
-return (0);
-}
+return (_strlen(s1) - _strlen(s2));
 }
 /**
  * _strlen - retorna el largo
