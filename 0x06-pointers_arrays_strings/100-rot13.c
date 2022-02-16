5d870ae66debc3801700e9596d8aa73d62fa6f18
EugeniaMatto <eugematto26@gmail.com>
2022-02-16 05:32:27 -0800

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,12 +5,22 @@
  */
 char *rot13(char *a)
 {
-int i = 0;
-      while (a[i] != '\0')
-      {
-	if (a[i] )
-	
-	      i++;
-      }
-        return (a);
+int i;
+for (i = 0; a[i] != '\0'; i++)
+{
+while ((a[i] >= 'a' && a[i] <= 'z')||(a[i] >= 'A' && a[i] <= 'Z'))
+{
+if ((a[i] >= 'a' && a[i] <= 'm')||(a[i] >= 'A' && a[i] <= 'M'))
+{
+a[i] = a[i] + 13;
+break;
+}
+else
+{
+a[i] = a[i] - 13;
+break;
+}
+}
+}
+return (a);
 }
