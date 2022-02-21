65cd790261d9fb4634ab271e64fda90cd27243e0
EugeniaMatto <eugematto26@gmail.com>
2022-02-21 14:23:52 -0800

    8 soy la mejor jejexd

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,25 +8,32 @@
  */
 void print_diagsums(int *a, int size)
 {
-int x, suma, sum, cont = 0;
+int x, y, suma, sum, cont = 0;
 x = 0;
+y = 0;
 while (cont < size)
 {
 
 	if (x == 0)
 	{
 	suma = 0;
+	sum = 0;
 	suma = a[0];
+	sum = a[size - 1];
 	x = 1 + size;
+	y = size + size - 2;
 	}
 	else
 	{
 	suma += a[x];
-	x += 1 + size;	  
+	sum += a[y];
+	x += 1 + size;
+	y += size - 1;	
 	}
 cont++;
 }
 printf("%d", suma);
+suma = 0;
+printf(", %d\n", sum); 
 sum = 0;
-printf(", %d\n", sum);  
 }
