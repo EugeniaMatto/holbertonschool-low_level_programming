84ea5724a6d8cb05f216b5f60b4132dbc80015aa
EugeniaMatto <eugematto26@gmail.com>
2022-02-21 08:34:46 -0800

    2 pfv

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,12 @@
  */
 char *_strchr(char *s, char c)
 {
-while ((*s != '\0') && (*s != c))
+while
+((*s != '\0') && (*s != c))
 {
 s++;
 }
-if (*s == '\0')
+if (*s == '\0' && c != '\0')
 return ('\0');
 return (s);
 }
