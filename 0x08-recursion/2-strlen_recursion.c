c2e86f1faa98d180caccd1b0dcaaa2967a02e316
EugeniaMatto <eugematto26@gmail.com>
2022-02-22 19:57:13 -0800

    3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - put en recursion
+ * @s: cadena
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (_strlen_recursion(s + 1) + 1);
+}
