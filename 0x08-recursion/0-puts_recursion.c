b75d89f36caefef6b0586fb75e68640e0b18fec3
EugeniaMatto <eugematto26@gmail.com>
2022-02-22 19:41:17 -0800

    0 fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,12 @@
  */
 void _puts_recursion(char *s)
 {
-
-if (*s == '\0')
+char c = *s;
+if (c == '\0')
 {
+_putchar('\n');
 return;
 }
-_putchar(*s);
-_puts_recursion(*s + 1);
+_putchar(c);
+_puts_recursion(s + 1);
 }
