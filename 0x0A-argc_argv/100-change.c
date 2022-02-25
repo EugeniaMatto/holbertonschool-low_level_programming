91fc151a707bf3c2eda4019ab55c2b3d067bbf73
EugeniaMatto <eugematto26@gmail.com>
2022-02-25 06:30:34 -0800

    ult fix betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 /**
  * main - das
- * @argc - cont argumentos
- * @argv - argumentos
+ * @argc: cont argumentos
+ * @argv: argumentos
+ * Return: sada
  */
 int main(int argc, char *argv[])
 {
-int mon, cent, i, total= 0;
+int mon, cent, i, total = 0;
 int array[5] = {25, 10, 5, 2, 1};
-if (argc != 2)
-{
-        printf("Error\n");
-        return (1);
-}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 cent = atoi(argv[1]);
-if (cent < 1)
-{
-printf("0\n");
-return (0);
-}
+	if (cent < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 i = 0;
 while (cent > 0 && i < 4)
 {
-mon = cent / (array[i]);
-cent = cent % (array[i]);
-total += mon;
-i++;
+	mon = cent / (array[i]);
+	cent = cent % (array[i]);
+	total += mon;
+	i++;
 }
 if (cent == 1)
 	total++;
