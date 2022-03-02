8e29db1b6f24a578db4b75cafb7429332e96fbce
EugeniaMatto <eugematto26@gmail.com>
2022-03-02 07:46:34 -0800

    test 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,13 +15,27 @@ if (width < 1 || height < 1)
 
 p = (int**)malloc(sizeof(int*) * (width));
 if (p == NULL)
+{
+	free(p);
 	return (NULL);
+}
+
 for (i = 0; i < width; i++)
 {
 	p[i] = (int*)malloc(height * sizeof(int));
 	if (p[i] == NULL)
-		return (NULL);
+	{
+		b = 1;
+	}
 }
+
+if (b == 1)
+{
+for (i = 0; i < width; i++)
+	free(p[i]);
+free(p);
+}
+b = 0;
 
 for (i = 0; i < width; i++)
 {
