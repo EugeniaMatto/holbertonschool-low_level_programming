5416aec7658f4ba1de9e23e8a02784bb6969350a
EugeniaMatto <eugematto26@gmail.com>
2022-03-02 05:22:38 -0800

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strdup - crea un array
+ * @str: tamaño del array
+ * Return: cadena
+ */
+char *_strdup(char *str)
+{
+char *p;
+int b, i;
+
+i = 0;
+while (str[i] != '\0')
+	i++;
+
+p = malloc(sizeof(char) * i);
+
+if (p == NULL || str[0] == '\0')
+	return ('\0');
+
+for (b = 0; b < i; b++)
+        p[b] = str[b];
+
+return (p);
+}
