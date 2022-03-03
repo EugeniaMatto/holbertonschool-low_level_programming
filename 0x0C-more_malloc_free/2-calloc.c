bbe97ae2e2be1fa0bf0f95c0b022a0c53461fdcf
EugeniaMatto <eugematto26@gmail.com>
2022-03-03 09:28:06 -0800

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,14 +7,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	unsigned int i;
+	char *p;
+	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(nmemb * size);
-	
+
 	if (p == NULL)
 		return (NULL);
 
-	return(p);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+
+	return (p);
 }
