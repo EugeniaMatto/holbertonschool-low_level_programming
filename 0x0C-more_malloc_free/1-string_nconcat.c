b48c707a1711242b150d1327a1f96b6161c70fa0
EugeniaMatto <eugematto26@gmail.com>
2022-03-03 05:54:33 -0800

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * string_nconcat - asda
+ * @s1: s1
+ * @s2: s2
+ * @n: n bytes de s2 concatenados a s1
+ * Return: puntero  a s1 + n bytes de s2
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int a, i, b, c = 0;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+i = 0;
+b = 0;
+while (s1[i] != '\0')
+i++;
+i++;
+while (s2[b] != '\0')
+b++;
+if (b < n)
+n = b;
+p = malloc(i + n);
+if (p == NULL)
+return (NULL);
+c = 0;
+b = 0;
+for (a = 0; a < (n + i - 1); a++)
+{
+if (s1[a] == '\0')
+b = 1;
+if (b == 0)
+p[a] = s1[a];
+if (b == 1)
+{
+p[a] = s2[c];
+c++;
+}
+}
+p[n + i] = '\0';
+return (p);
+}
