9927a4fc1aebeaab079e6df62939ec3ba2230237
EugeniaMatto <eugematto26@gmail.com>
2022-03-08 07:41:13 -0800

    fix 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,7 +5,7 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL || d->name == NULL || d->owner == NULL)
+	if (d == NULL || d->name == NULL || d->owner == NULL || d->age < 1)
 		return;
 
 	printf("Name: %s\nAge: %f\nOwner:%s\n", d->name, d->age, d->owner);
