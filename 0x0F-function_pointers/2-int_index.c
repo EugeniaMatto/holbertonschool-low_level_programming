0bee8fa29e3c980e03bf52a4fe0d28493cc6b851
EugeniaMatto <eugematto26@gmail.com>
2022-03-09 14:57:54 -0800

    test 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,16 +8,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = -1;
+	int i;
 
 	if (array && cmp && size >= 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]) != 0)
+			if ((cmp)(array[i]) != 0)
 			return (i);
 		}
 	}
 
-	return (i);
+	return (-1);
 }
