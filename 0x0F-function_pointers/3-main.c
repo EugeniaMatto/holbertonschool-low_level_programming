fa8c2e9a459127c13145fe11ade9c16dcbb5f384
EugeniaMatto <eugematto26@gmail.com>
2022-03-09 09:29:14 -0800

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit (98);
-		return (98);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	printf("%d\n", (*get_op_func(argv[2]))(a, b));
 
+	printf("%d\n", (*get_op_func(argv[2]))(a, b));
+	
 	return (0);
 }
