ad5bb5c5e15aeb57c51c61cf6d65908dff19d7b8
EugeniaMatto <eugematto26@gmail.com>
2022-03-10 07:50:13 -0800

    test

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 
-		if ((x != n - 1) && (separador != NULL))
+		if ((x != n - 1) && (separator != NULL))
 			printf("%s", separator);
 	}
 	va_end(lista);
