8fbf236e53c07a1e64b55d60429f423db597f045
EugeniaMatto <eugematto26@gmail.com>
2022-03-10 07:23:40 -0800

    fix 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
 /**
  * sum_them_all - sum all arguments
  * @n: args (int)
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,7 +11,6 @@ typedef struct op
 	char *op;
 	char *type;
 } op_t;
-#include <stdlib.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
