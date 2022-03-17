2a2765a50f1cd37ea50beec2a36613d3bd662f33
EugeniaMatto <eugematto26@gmail.com>
2022-03-17 10:28:17 -0700

    clean repo

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -20,7 +20,16 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(2);
-	}
 	printf("55 48 89 e5 48 83 ec 30 89 7d dc 48 89 75 d0 83 7d dc 02 74 14");
+
+    ud_init(&ud_obj);
+    ud_set_input_file(&ud_obj, stdin);
+    ud_set_mode(&ud_obj, 64);
+    ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+    while (ud_disassemble(&ud_obj)) {
+        printf("\t%s\n", ud_insn_asm(&ud_obj));
+    }
+
 return (0);
 }
