4fbda9e7d7417f725e6eb3e809823340c1d105c1
EugeniaMatto <eugematto26@gmail.com>
2022-03-17 14:41:18 -0700

    ex 2 test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <string.h>
+char *_strdup(char *str);
 #include <stdlib.h>
 /**
  * add_node -  adds a new node at the beginning of a list_t list.
@@ -11,6 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	size_t i = 0;
 	list_t *new;
+	char *aux = (char *)str;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -18,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->str = strdup(str);
+	new->str = _strdup(aux);
 	while (str)
 		i++;
 	new->len = i;
@@ -26,4 +27,28 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new;
 
 	return (new);
+}
+/**
+ * _strdup - crea un array
+ * @str: tamaño del array
+ * Return: cadena
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int b, i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	p = malloc(sizeof(char) * i);
+
+	if (p == NULL || str[0] == '\0')
+		return ('\0');
+
+	for (b = 0; b < i; b++)
+		p[b] = str[b];
+
+	return (p);
 }
