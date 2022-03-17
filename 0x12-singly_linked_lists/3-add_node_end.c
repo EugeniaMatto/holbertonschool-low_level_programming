404b47f0f991972a289536cbfaf103219f74b974
EugeniaMatto <eugematto26@gmail.com>
2022-03-17 15:48:57 -0700

    fixed ex 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+char *_strdup(char *str);
+#include <stdlib.h>
+/**
+ * add_node_end -  adds a new node at the end of a list_t list.
+ * @head: first node
+ * @str: string a colocar en el nuevo nodo
+ * Return: pointer to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t i = 0;
+	list_t *old;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = _strdup((char *)str);
+
+	while (str[i] != '\0')
+		i++;
+
+	new->len = i;
+	new->next = NULL;
+
+	if (*head != NULL)
+	{
+		old = *head;
+		while (old->next != NULL)
+			old = old->next;
+
+		old->next = new;
+	}
+	else
+		(*head) = new;
+
+	return (new);
+}
+/**
+ * _strdup - copia un array
+ * @str: array
+ * Return: array copiado
+ */
+char *_strdup(char *str)
+{
+char *p;
+int b, i;
+
+if (str == NULL)
+return (NULL);
+
+i = 0;
+while (str[i] != '\0')
+i++;
+
+p = malloc(sizeof(char) * i);
+
+if (p == NULL)
+return (NULL);
+
+for (b = 0; b < i; b++)
+p[b] = str[b];
+
+return (p);
+}
