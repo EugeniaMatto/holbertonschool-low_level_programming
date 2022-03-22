d4cb57414c943de7ed2da028d8da8877194490c5
EugeniaMatto <eugematto26@gmail.com>
2022-03-22 11:50:38 -0700

    fix exe 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		aux = *head;
-		*head = (*head)->next;
-		free(aux);
+		while (*head != NULL)
+		{
+			aux = *head;
+			*head = (*head)->next;
+			free(aux);
+		}
 	}
 }
