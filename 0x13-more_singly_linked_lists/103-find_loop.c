06b41750c6668a63af2232e447999394359c2693
EugeniaMatto <eugematto26@gmail.com>
2022-03-22 16:37:47 -0700

    103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+listint_t *find_listint_loop(listint_t *head);
+/**
+ * find_listint_loop -  function that finds the loop in a linked list.
+ * @head: head
+ * Return: The address of the node loop starts, or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *aux = head, *aux2 = head;
+
+	while (aux != NULL)
+	{
+		if (aux->next == (head))
+			return (aux);
+
+		aux2 = head;
+		while (aux2 != NULL)
+		{
+			if ((aux->next == aux2->next) && (aux != aux2))
+				return (aux);
+
+			aux2 = aux2->next;
+		}
+		aux = aux->next;
+	}
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,8 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+listint_t *find_listint_loop(listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
