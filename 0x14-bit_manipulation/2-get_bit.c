43a325223e2503a3191e2662b8dc7da54611f0c2
EugeniaMatto <eugematto26@gmail.com>
2022-03-24 13:29:31 -0700

    fixed 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,12 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if ((n >> index) > 0)
-	{
+	if (((n >> index) & 1) == 0 || ((n >> index) & 1) == 1)
 		return ((n >> index) & 1);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (-1);
 }
