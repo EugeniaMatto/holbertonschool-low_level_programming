2a57d8f65fb580fda54e89e924e1f541add5999e
EugeniaMatto <eugematto26@gmail.com>
2022-03-28 11:54:18 -0700

    fixed 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,6 +23,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	lec = read(fd, bufer, letters);
+	if (lec == -1)
+		return (0);
+
 	ret = write(1, bufer, lec);
 
 	free(bufer);
