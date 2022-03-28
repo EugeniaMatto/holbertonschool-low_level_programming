85ce66b1acda64e8ef0fabeb0d85684a69fae1d6
EugeniaMatto <eugematto26@gmail.com>
2022-03-28 11:37:26 -0700

    .h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include<sys/types.h>
 #include<sys/stat.h>
 #include <fcntl.h>
@@ -8,3 +11,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+#endif
+
