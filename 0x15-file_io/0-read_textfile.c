8d9d81315f33694cd1e312790e3a83535ab5c35a
EugeniaMatto <eugematto26@gmail.com>
2022-03-28 11:31:00 -0700

    exe 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints to stdout
+ * @filename: pointer to name file
+ * @letters:  the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, ret;
+	char *bufer;
+
+	if (filename == NULL)
+		return (0);
+
+	bufer = malloc(letters * sizeof(char));
+	if (bufer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	read(fd, bufer, letters);
+	ret = write(1, bufer, letters);
+
+	close(fd);
+	if (ret != (ssize_t) letters)
+		return (0);
+
+	return (ret);
+}
