561a33f4ea4431c5901e21ef1c6415e887fb3553
EugeniaMatto <eugematto26@gmail.com>
2022-03-28 15:18:01 -0700

    fixed exe 3, error 100

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+void error97(void);
+void close100(int file);
 /**
  * main -Write a program that copies the content of a file to another file.
  * @argc: argument count
@@ -12,10 +14,7 @@ int main(int argc, char **argv)
 	int fd_from, fd_to, rd;
 
 	if (argc != 3)
-	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-		exit(97);
-	}
+		error97();
 	file_from = argv[1];
 	file_to = argv[2];
 
@@ -38,13 +37,36 @@ int main(int argc, char **argv)
 		rd = read(fd_from, buffer, 1024);
 		if ((rd == -1) || (fd_from == -1))
 		{
-			close(fd_to);
-			close(fd_from);
+			close100(fd_to);
+			close100(fd_from);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
 		write(fd_to, buffer, rd);
 	}
-	close(fd_from);
+	close100(fd_from);
 	return (0);
+}
+/**
+ * error97 -  error97
+ */
+void error97(void)
+{
+	write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
+	exit(97);
+}
+
+/**
+ * close100 - error al cerrar file
+ * @file: file a cerrar
+ */
+void close100(int file)
+{
+	int a = close(file);
+
+	if (a == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file);
+		exit (100);
+	}
 }
