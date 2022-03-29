c524e5fb40da905d348a468709c30f9b09d60105
EugeniaMatto <eugematto26@gmail.com>
2022-03-28 18:05:51 -0700

    100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,14 @@
+#include "main.h"
+
+#include <elf.h>
+
+#include <string.h>
+#include <stdio.h>
+#if defined(__LP64__)
+#define ElfW(type) Elf64_ ## type
+#else
+#define ElfW(type) Elf32_ ## type
+#endif
 /**
  * main -Write a program that copies the content of a file to another file.
  * @argc: argument count
@@ -6,6 +17,28 @@
  */
 int main(int argc, char **argv)
 {
-	argv[0] = "";
-	return (argc);
+	ElfW(Ehdr) header;
+	char *file;
+	int fd;
+
+	if (argc != 2)
+	{
+		write(STDERR_FILENO, "Error\n", 98);
+		exit(98);
+	}
+	file = argv[1];
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		write(STDERR_FILENO, "Error\n", 98);
+		exit(98);
+	}
+	if ((memcmp(header.e_ident, ELFMAG, SELFMAG) == 0))
+	{
+	printf(":p");
+	}
+
+	write(STDERR_FILENO, "Error\n", 98);
+	exit(98);
 }
