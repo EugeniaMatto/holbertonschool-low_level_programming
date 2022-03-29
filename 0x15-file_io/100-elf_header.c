5b6922d17a992c20f447bc7759c3e2e5a05fda9d
EugeniaMatto <eugematto26@gmail.com>
2022-03-28 17:04:17 -0700

    test 100

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,11 @@
+/**
+ * main -Write a program that copies the content of a file to another file.
+ * @argc: argument count
+ * @argv: argument list
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	argv[0] = "";
+	return (argc);
+}
