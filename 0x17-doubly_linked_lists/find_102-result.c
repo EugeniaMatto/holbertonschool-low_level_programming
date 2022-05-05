2c9754e9038c375fb0872d7bb15a9e009d203975
EugeniaMatto <eugematto26@gmail.com>
2022-05-05 11:51:00 -0700

    exe 102

--- 0x17-doubly_linked_lists/find_102-result.c
+++ 0x17-doubly_linked_lists/find_102-result.c
@@ -3,26 +3,27 @@
 void main (void) 
 {
 	unsigned long i = 998001; /* 999 * 999 */
-	unsigned long a = 999, b = 999;
+	unsigned long a = 999, b = 999, max = 0;
 	unsigned long res1 = 0, res2 = 1, n = 0;
 
-	for (; a > 99; a--, b--)
+	for (; a > 99; a--)
 	{
-		i = a * b;
-		res1 = i / 1000;
-		n = i;
-		res2 = (n % 10) * 100;
-		n = i / 10;
-		res2 += (n % 10) * 10;
-		n = n / 10;
-		res2 += (n % 10);
-
-		if (res1 == res2)
+		for (b = 999; b > 99; b--)
 		{
-			printf("res de multiplicar: %lu * %lu\n", a, b);
-			printf("%lu\n", i);
-			return;
+			i = a * b;
+			res1 = i / 1000;
+			n = i;
+			res2 = (n % 10) * 100;
+			n = i / 10;
+			res2 += (n % 10) * 10;
+			n = n / 10;
+			res2 += (n % 10);
+			if (res1 == res2)
+			{
+				if (max < i)
+					max = i;
+			}
 		}
 	}
-	printf("No se encontro\n");
+	printf("%lu\n", max);
 }
