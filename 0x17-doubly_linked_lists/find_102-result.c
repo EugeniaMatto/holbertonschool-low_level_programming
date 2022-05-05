eccaa0fbb69a695fe74ede716e31819686ba4c85
EugeniaMatto <eugematto26@gmail.com>
2022-05-05 11:28:56 -0700

    ex 102

---
+++ 0x17-doubly_linked_lists/find_102-result.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+void main (void) 
+{
+	unsigned long i = 998001; /* 999 * 999 */
+	unsigned long res1 = 0, res2 = 1, n = 0;
+
+	for (; res1 != res2 && i > 100000 ; i--)
+	{
+		res1 = i / 1000;
+		n = i;
+		res2 = (n % 10);
+		n = i / 10;
+		res2 += (n % 10) * 10;
+		n = n / 10;
+		res2 += (n % 10) * 100;
+
+		if (res1 == res2)
+		{
+			printf("%lu\n", i);
+			return;
+		}
+	}
+	printf("No se encontro\n");
+}
