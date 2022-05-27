00079b1ebcee2aced3d04e499eebbf77c96d0ad4
EugeniaMatto <eugematto26@gmail.com>
2022-05-27 12:01:41 -0700

    ex 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *a, *t;
+
+	if(ht)
+	{
+		for (; i < ht->size; i++)
+		{
+			a = ht->array[i];
+			while(a)
+			{
+				t = a->next;
+				free(a->key);
+				free(a->value);
+				free(a);
+				a = t;
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
