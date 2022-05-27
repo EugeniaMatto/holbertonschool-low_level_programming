202f229c946f06ca6c38cb7d0c654cd4007cb5a6
EugeniaMatto <eugematto26@gmail.com>
2022-05-27 11:18:13 -0700

    ex 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: is the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0, z = 0;
+	hash_node_t *aux;
+
+	printf("{");
+	for (; ht && i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		while (aux)
+		{
+			if (z == 1)
+				printf(",");
+			z = 1;
+			printf("'%s': '%s'");
+			aux = aux->next;
+		}
+	}
+	printf("}\n");
+}
