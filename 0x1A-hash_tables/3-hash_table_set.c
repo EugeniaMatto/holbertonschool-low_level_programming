19d0c5f567cdd2d2b75f19c916206dcb07ebdf8a
EugeniaMatto <eugematto26@gmail.com>
2022-05-27 10:59:40 -0700

    betty fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,7 +4,7 @@ hash_node_t *new_node(hash_node_t *old);
  * hash_table_set - function that adds an element to the hash table.
  * @ht: the hash table you want to add or update the key/value to
  * @key: the key. key can not be an empty string
- * @value: the value associated with the key. value must be duplicated. value can be an empty string.
+ * @value: the value associated with the key.
  * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -44,6 +44,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	return (1);
 }
 
+/**
+ *  new_node - adds new node
+ *  @old - data
+ *  Return: new node
+ */
 hash_node_t *new_node(hash_node_t *old)
 {
 	hash_node_t *new = malloc(sizeof(hash_node_t));
