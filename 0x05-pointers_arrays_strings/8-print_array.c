64fa821c98846bab715bab9663a0826f5beace3b
Eugenia Matto <98404484+EugeniaMatto@users.noreply.github.com>
2022-06-10 23:45:44 -0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,12 +7,13 @@
  */
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n; i++)
-{
-printf("%d", a[i]);
-if (i != (n - 1))
-printf(", ");
-}
-putchar('\n');
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+        if (i != (n - 1))
+            printf(", ");
+    }
+    putchar('\n');
 }
