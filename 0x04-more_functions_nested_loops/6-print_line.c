e2aa6f57ec7b8c5e54b688129d959f6ab2b76a2a
Eugenia Matto <98404484+EugeniaMatto@users.noreply.github.com>
2022-06-10 23:44:27 -0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,10 +5,9 @@
  */
 void print_line(int n)
 {
-int i;
-for (i = 0; i < n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
+    int i;
+
+    for (i = 0; i < n; i++)
+        _putchar('_');
+    _putchar('\n');
 }
