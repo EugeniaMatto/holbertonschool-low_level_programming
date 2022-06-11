a5ecb693f4546a1a35ab150950fe0e79241f56df
Eugenia Matto <98404484+EugeniaMatto@users.noreply.github.com>
2022-06-10 23:41:56 -0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,28 +5,30 @@
  */
 int main(void)
 {
-int i;
-int x = 0;
-for (i = 1; i <= 100; i++)
-{
-if (((i % 10) + (i / 10)) % 3 == 0)
-{
-printf("Fizz");
-x = 1;
-}
-if ((i % 10 == 5) || (i % 10 == 0))
-{
-printf("Buzz");
-x = 1;
-}
-if (x == 0)
-printf("%d", i);
-x = 0;
-if (i != 100)
-{
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+    int i;
+    int x = 0;
+    
+    for (i = 1; i <= 100; i++)
+    {
+        if (((i % 10) + (i / 10)) % 3 == 0)
+        {
+            printf("Fizz");
+            x = 1;
+        }
+      
+        if ((i % 10 == 5) || (i % 10 == 0))
+        {
+            printf("Buzz");
+            x = 1;
+        }
+      
+        if (x == 0)
+            printf("%d", i);
+      
+        x = 0;
+        if (i != 100)
+            putchar(' ');
+    }
+    putchar('\n');
+    return (0);
 }
